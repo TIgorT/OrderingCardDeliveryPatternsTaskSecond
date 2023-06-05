e3f7702697a9d577b5b057a0ac733e30338b782e
Igor <TimoshenkoII@yandex.com>
2023-06-05 18:03:13 +0300

    Changes commit

--- src/test/java/ru/netology/testmode/test/AuthTest.java
+++ src/test/java/ru/netology/testmode/test/AuthTest.java
@@ -1,9 +1,13 @@
 package ru.netology.testmode.test;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static ru.netology.testmode.data.DataGenerator.Registration.getRegisteredUser;
 import static ru.netology.testmode.data.DataGenerator.Registration.getUser;
@@ -21,25 +25,37 @@ class AuthTest {
     @DisplayName("Should successfully login with active registered user")
     void shouldSuccessfulLoginIfRegisteredActiveUser() {
         var registeredUser = getRegisteredUser("active");
-        // TODO: добавить логику теста, в рамках которого будет выполнена попытка входа в личный кабинет с учётными
-        //  данными зарегистрированного активного пользователя, для заполнения полей формы используйте
-        //  пользователя registeredUser
+
+        $("[data-test-id='login'] .input__control").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] .input__control").setValue(registeredUser.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("h2").shouldHave(Condition.exactText("Личный кабинет")).shouldBe(Condition.visible);
     }
 
     @Test
     @DisplayName("Should get error message if login with not registered user")
     void shouldGetErrorIfNotRegisteredUser() {
         var notRegisteredUser = getUser("active");
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет
-        //  незарегистрированного пользователя, для заполнения полей формы используйте пользователя notRegisteredUser
+
+        $("[data-test-id='login'] .input__control").setValue(notRegisteredUser.getLogin());
+        $("[data-test-id='password'] .input__control").setValue(notRegisteredUser.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldHave(Condition.exactText("Ошибка! Неверно указан логин или пароль"), Duration.ofSeconds(5000))
+                .shouldBe(Condition.visible);
     }
 
     @Test
     @DisplayName("Should get error message if login with blocked registered user")
     void shouldGetErrorIfBlockedUser() {
         var blockedUser = getRegisteredUser("blocked");
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет,
-        //  заблокированного пользователя, для заполнения полей формы используйте пользователя blockedUser
+
+        $("[data-test-id='login'] .input__control").setValue(blockedUser.getLogin());
+        $("[data-test-id='password'] .input__control").setValue(blockedUser.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldHave(Condition.exactText("Ошибка! Пользователь заблокирован"), Duration.ofSeconds(5000))
+                .shouldBe(Condition.visible);
     }
 
     @Test
@@ -47,9 +63,26 @@ class AuthTest {
     void shouldGetErrorIfWrongLogin() {
         var registeredUser = getRegisteredUser("active");
         var wrongLogin = getRandomLogin();
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет с неверным
-        //  логином, для заполнения поля формы "Логин" используйте переменную wrongLogin,
-        //  "Пароль" - пользователя registeredUser
+
+        $("[data-test-id='login'] .input__control").setValue(wrongLogin);
+        $("[data-test-id='password'] .input__control").setValue(registeredUser.getLogin());
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldHave(Condition.exactText("Ошибка! Неверно указан логин или пароль"), Duration.ofSeconds(5000))
+                .shouldBe(Condition.visible);
+    }
+
+    @Test
+    @DisplayName("The message Field must be filled in should appear")
+    void shouldGetMessageFieldMustBeFilledInShouldAppearLogin() {
+        var registeredUser = getRegisteredUser("active");
+
+        $("[data-test-id='login'] .input__control").setValue("");
+        $("[data-test-id='password'] .input__control").setValue(registeredUser.getLogin());
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='login'] .input__sub")
+                .shouldHave(Condition.exactText("Поле обязательно для заполнения"), Duration.ofSeconds(5000))
+                .shouldBe(Condition.visible);
     }
 
     @Test
@@ -57,8 +90,41 @@ class AuthTest {
     void shouldGetErrorIfWrongPassword() {
         var registeredUser = getRegisteredUser("active");
         var wrongPassword = getRandomPassword();
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет с неверным
-        //  паролем, для заполнения поля формы "Логин" используйте пользователя registeredUser,
-        //  "Пароль" - переменную wrongPassword
+
+        $("[data-test-id='login'] .input__control").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] .input__control").setValue(wrongPassword);
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldHave(Condition.exactText("Ошибка! Неверно указан логин или пароль"), Duration.ofSeconds(5000))
+                .shouldBe(Condition.visible);
     }
+
+    @Test
+    @DisplayName("The message Field must be filled in should appear")
+    void shouldGetMessageFieldMustBeFilledInShouldAppearPassword() {
+        var registeredUser = getRegisteredUser("active");
+
+        $("[data-test-id='login'] .input__control").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] .input__control").setValue("");
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='password'] .input__sub")
+                .shouldHave(Condition.exactText("Поле обязательно для заполнения"), Duration.ofSeconds(5000))
+                .shouldBe(Condition.visible);
+    }
+
+    @Test
+    @DisplayName("The message field must be filled in should appear")
+    void shouldGetMessageFieldMustBeFilledInShouldAppearInForm() {
+
+        $("[data-test-id='login'] .input__control").setValue("");
+        $("[data-test-id='password'] .input__control").setValue("");
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='login'] .input__sub")
+                .shouldHave(Condition.exactText("Поле обязательно для заполнения"), Duration.ofSeconds(5000))
+                .shouldBe(Condition.visible);
+        $("[data-test-id='password'] .input__sub")
+                .shouldHave(Condition.exactText("Поле обязательно для заполнения"), Duration.ofSeconds(5000))
+                .shouldBe(Condition.visible);
+    }
+
 }
